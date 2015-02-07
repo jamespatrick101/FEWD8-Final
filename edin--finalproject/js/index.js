b27d871bb549c72aa03b817677a1c9d1e204b20d
j.ed <jamespatrick101@gmail.com>
2015-02-07 12:34:56 -0500

    initial commit
    add my initial project files

---
+++ edin--finalproject/js/index.js
@@ -0,0 +1,29 @@
+
+$(document).ready(function(){
+  $('.slick-carousel').slick({
+    autoplay: true,
+    autoplaySpeed: 600
+  });
+});
+
+
+$('.hamburger').on('click', function() {
+	$('.main-nav2').slideToggle();
+});
+
+
+// $(document).ready(function(){
+//   $('#map').jMapping();
+// });
+
+
+// $.shifter({
+// 	maxWidth: Infinity
+// });
+
+
+// jQuery(function($){
+// 	     $( '.hamburger' ).click(function(){
+// 		     $('.main-nav2').toggleClass('expand')
+// 	     })
+//     })
